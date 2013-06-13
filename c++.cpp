be6997d078a69545020a3c79e10f025d77dadd71
Mriganka Basu Roy Chowdhury <mbrc12@gmail.com>
2013-06-13 18:18:42 +0530

    C++ Standard Template File

---
+++ c++.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <functional>
+#include <bitset>
+#include <set>
+#include <map>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#define MAX(a,b) (((a)>(b))?(a):(b))
+#define MIN(a,b) (((a)<(b))?(a):(b))
+
+using namespace std;
+
+int main()
+{
+
+}
